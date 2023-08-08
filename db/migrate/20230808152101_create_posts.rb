9e0f99020e9c0ce0f9d721d621e3b9186adc1566
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-08-08 15:30:11 +0000

    [Add]modelファイル作成

---
+++ db/migrate/20230808152101_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :category, foreign_key: true, null: false
+      t.string     :name, null: false
+      t.string     :post_title, null: false
+      t.text       :post_comment, null: false
+      t.integer    :favorite, default: 0
+      t.date       :visited_date, null: false, default: -> { 'CURRENT_DATE' }
+      t.boolean    :is_private, null: false, default: false
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/post_test.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id           :integer          not null, primary key
+#  favorite     :integer          default(0)
+#  is_private   :boolean          default(FALSE), not null
+#  name         :string           not null
+#  post_comment :text             not null
+#  post_title   :string           not null
+#  visited_date :date             default(NULL), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  category_id  :integer          not null
+#  user_id      :integer          not null
+#
+# Indexes
+#
+#  index_posts_on_category_id  (category_id)
+#  index_posts_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  category_id  (category_id => categories.id)
+#  user_id      (user_id => users.id)
+#
+require "test_helper"
+
+class PostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
