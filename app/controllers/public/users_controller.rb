9b2ab4e8002bd671240c88e6ca8c85a21355587e
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-08-25 16:57:52 +0000

    [fix]MAP表示

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -5,7 +5,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @posts = @user.posts.order(created_at: :desc)
-    @pagy, @posts = pagy(@posts, items: 9)
+    @pagy, @posts = pagy(@posts, items: 10)
 
   end
 
